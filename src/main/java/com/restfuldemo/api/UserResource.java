e7e4507ab1a4e512a77805708df0d70981f59b21
TroyLiu <asasliu_2006@126.com>
2017-01-16 21:48:00 +0800

    增加 profile，不同构建环境

--- src/main/java/com/restfuldemo/api/UserResource.java
+++ src/main/java/com/restfuldemo/api/UserResource.java
@@ -3,11 +3,14 @@ package com.restfuldemo.api;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import com.restfuldemo.util.ConfigUtil;
 @Path(value="/user")
 @Produces("text/plain")
 public class UserResource {
 	@GET
 	public String getUserInfo(){
+		System.out.println(ConfigUtil.get("database.jdbc.connectionURL"));
 		return "雾神带我飞";
 	}
 }
---
+++ src/main/java/com/restfuldemo/util/ConfigUtil.java
@@ -0,0 +1,25 @@
+package com.restfuldemo.util ;
+
+import java.util.ResourceBundle;
+
+/**
+ * 项目参数工具类
+ * 
+ * @author TroyLiu
+ * 
+ */
+public class ConfigUtil {
+
+	private static final ResourceBundle bundle = java.util.ResourceBundle.getBundle("service.properties");
+
+	/**
+	 * 通过键获取值
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public static final String get(String key) {
+		return bundle.getString(key);
+	}
+
+}
